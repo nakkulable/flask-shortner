cfb72f1388f6fc9ade6aff6319342361a11e9ec8
nakulv <nakul@bidtech.co>
2014-11-04 20:17:19 +0530

    [MODIFIED] temp hack for port.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 127.0.0.1:33507 run:app
+web: python run.py
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import app
-from werkzeug.contrib.fixers import ProxyFix
+# from werkzeug.contrib.fixers import ProxyFix
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
-
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 # app.run(debug=True)
