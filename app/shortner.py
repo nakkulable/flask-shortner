9d9d1cd569a3462f5268d4c57d4d7f781df46240
nakulv <nakul@bidtech.co>
2014-11-04 18:23:15 +0530

    [ADDED] Initial Commit to shortner.

---
+++ app/shortner.py
@@ -0,0 +1,51 @@
+import redis
+import base64
+import md5
+import config
+
+
+class UrlShortener:
+    def __init__(self):
+        self.redis = redis.StrictRedis(host=config.REDIS_HOST,
+                                       port=config.REDIS_PORT,
+                                       db=config.REDIS_DB)
+
+    def shortcode(self, url):
+        """
+        for URLs the potential for collision in the 32 last bits
+        of the MD5 hash is rather unlikely.
+        The following things happen, in order:
+        * compute the md5 digest of the given source
+        * extract the lower 4 bytes
+        * base64 encode the result
+        * remove trailing padding if it exists
+        Of course, should a collision happen, we will evict the previous
+        key.
+        """
+        return base64.b64encode(
+            md5.new(url).digest()[-4:]).replace('=', '').replace('/', '_')
+
+    def shorten(self, url):
+        """
+        Set the value of the url to the key.
+        Minimal workflow to this.
+        Return a dictionary(always xD)
+        """
+        code = self.shortcode(url)
+        try:
+            self.redis.set(config.REDIS_PREFIX + code, url)
+            return {'success': True,
+                    'url': url,
+                    'code': code,
+                    'shorturl': config.URL_PREFIX + code}
+        except:
+            return {'success': False}
+
+    def lookup(self, code):
+        """
+        Lookup the key and return None if the key isn't present
+        """
+        try:
+            return self.redis.get(config.REDIS_PREFIX + code)
+        except:
+            return None
