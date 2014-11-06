6028c88248ea8f3eef9f9c3d26a7120be48e062d
nakulv <nakul@bidtech.co>
2014-11-06 23:44:26 +0530

    [MODIFIED] Instantiating the repo and working on the views.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from . import app
+from flask import json, request, redirect, render_template, make_response
+from shortner import UrlShortener
+import urlparse
+
+shrt = UrlShortener()
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
-    return "Meenakshi I love you!"
+    return render_template('index.html')
